20870faa74322fcbc355dc1c579fe5681720b387
Saumya Bhatnagar <saumya.bhatnagar.sb@gmail.com>
2019-02-28 22:28:13 +0530

    finish path matrix screen layout

--- app/src/main/java/com/aakankshareddy/sihpassengerapp/Paths.java
+++ app/src/main/java/com/aakankshareddy/sihpassengerapp/Paths.java
@@ -12,12 +12,6 @@ public class Paths extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paths);
-
-        String[] paths = {"path 1", "path 2", "path 3"};
-
-        ListView pathsListView = findViewById(R.id.paths_matrix_list_view);
-        ArrayAdapter<String> pathsAdapter = new ArrayAdapter<>(this, R.layout.paths_list_item, paths);
-        pathsListView.setAdapter(pathsAdapter);
     }
 
 
