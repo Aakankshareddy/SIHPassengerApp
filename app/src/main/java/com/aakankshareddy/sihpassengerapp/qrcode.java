a5a63f2a1ea5881f5c00b2fb23678c71aab56e4e
Jap-Leen <jap.jolly@gmail.com>
2019-03-03 10:15:27 +0530

    ädded qr code generation

--- app/src/main/java/com/aakankshareddy/sihpassengerapp/qrcode.java
+++ app/src/main/java/com/aakankshareddy/sihpassengerapp/qrcode.java
@@ -4,24 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
-public class BusView extends AppCompatActivity {
+public class qrcode extends AppCompatActivity {
     private Button Proceed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bus_matrix_view);
-        Proceed = (Button) findViewById(R.id.proceedtopay);
+        setContentView(R.layout.activity_qrcode);
+        Proceed = (Button) findViewById(R.id.proceedfromqr);
         Proceed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i=new Intent(
-                        BusView.this,
-                        qrcode.class);
+                        qrcode.this,
+                        PaymentsView.class);
                 startActivity(i);
             }
 
         });
     }
+
 }
